c2b435f7c5fd6542939a7bae88bc1c205f9c9918
abrka <ifradnashit@gmail.com>
2025-11-03 14:56:16 +0600

    update example project

--- example/assets/script.js
+++ example/assets/script.js
@@ -1,12 +1,27 @@
-fetch('/api_call', {
-  method: 'GET'
-})
-  .then(function (response) { return response.json(); })
-  .then(function (json) {
-    console.log(json);
-  });
-
-let file = "fetch_info.txt"
-fetch(file)
+fetch("fetch_info.txt")
   .then(x => x.text())
   .then(y => document.getElementById("demo").innerHTML = y);
+
+
+fetch("/files", {
+  method: 'GET'
+})
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (json) {
+    console.log(json);
+    
+    const myList = document.getElementById('filename-list');
+    console.log("p1");
+    for (let index = 0; index < json["filenames"].length; index++) {
+      console.log("p2");
+      const element = json["filenames"][index];
+      const filename = "filename is " + element["filename"];
+      const newListItem = document.createElement('li');
+      newListItem.textContent = filename;
+      myList.appendChild(newListItem);
+    }
+  });
+
+
