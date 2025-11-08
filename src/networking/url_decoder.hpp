650490b2c7d48036cabffc2d7b3419eebfb89063
abrka <ifradnashit@gmail.com>
2025-11-08 15:20:18 +0600

    fix url decoder

--- src/networking/url_decoder.hpp
+++ src/networking/url_decoder.hpp
@@ -5,20 +5,49 @@
 #include <string>
 
 namespace HTTP {
-// encoding : decoding
-static std::map<std::string, std::string> url_decode_map{
-    {R"(%3A)", ":"}, {R"(%3A)", ":"}, {R"(%2F)", "/"}, {R"(%3F)", "?"},
-    {R"(%23)", "#"}, {R"(%5B)", "["}, {R"(%5D)", "]"}, {R"(%40)", "@"},
-    {R"(%21)", "!"}, {R"(%24)", "$"}, {R"(%26)", "&"}, {R"(%27)", "'"},
-    {R"(%28)", "("}, {R"(%29)", ")"}, {R"(%2A)", "*"}, {R"(%2B)", "+"},
-    {R"(%2C)", ","}, {R"(%3B)", ";"}, {R"(%3D)", "="}, {R"(%25)", "%"},
-    {R"(%20)", " "}, {R"(\+)", " "}};
 
-std::string url_decode(const std::string &str) {
-  std::string output = str;
-  for (const auto &[encoding, decoding] : url_decode_map) {
-    output = std::regex_replace(output, std::regex(encoding), decoding);
+int url_decode_raw(char *out, const char *in) {
+    static const char tbl[256] = {
+        -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+        -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+        -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 0,  1,  2,  3,  4,  5,
+        6,  7,  8,  9,  -1, -1, -1, -1, -1, -1, -1, 10, 11, 12, 13, 14, 15, -1,
+        -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+        -1, -1, -1, -1, -1, -1, -1, 10, 11, 12, 13, 14, 15, -1, -1, -1, -1, -1,
+        -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+        -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+        -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+        -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+        -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+        -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+        -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+        -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+        -1, -1, -1, -1};
+    char c, v1, v2, *beg = out;
+    if (in != NULL) {
+      while ((c = *in++) != '\0') {
+        if (c == '%') {
+          if ((v1 = tbl[(unsigned char)*in++]) < 0 ||
+              (v2 = tbl[(unsigned char)*in++]) < 0) {
+            *beg = '\0';
+            return -1;
+          }
+          c = (v1 << 4) | v2;
+        }
+        c = (c == '+') ? ' ' : c; // replace + with whitespace
+        *out++ = c;
+      }
+    }
+    *out = '\0';
+    return 0;
   }
-  return output;
-}
+
+  std::string url_decode(const std::string &str) {
+    char* out_c_str = (char*)malloc(str.size() + 1);
+    int ret = url_decode_raw(out_c_str, str.c_str());
+    assert(ret == 0);
+    std::string out_str{out_c_str};
+    free(out_c_str);
+    return out_str;
+  }
 } // namespace HTTP
