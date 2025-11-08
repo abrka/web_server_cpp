e4e661197b0e65367a590f75fd1c0269ba4502da
abrka <ifradnashit@gmail.com>
2025-11-08 14:04:33 +0600

    put url decoder in http namespace

--- src/networking/url_decoder.hpp
+++ src/networking/url_decoder.hpp
@@ -1,39 +1,24 @@
 #pragma once
 
 #include <map>
-#include <string>
 #include <regex>
+#include <string>
 
+namespace HTTP {
 // encoding : decoding
 static std::map<std::string, std::string> url_decode_map{
-  {R"(%3A)",":"},
-  {R"(%3A)",":"},
-  {R"(%2F)","/"},
-  {R"(%3F)","?"},
-  {R"(%23)","#"},
-  {R"(%5B)","["},
-  {R"(%5D)","]"},
-  {R"(%40)","@"},
-  {R"(%21)","!"},
-  {R"(%24)","$"},
-  {R"(%26)","&"},
-  {R"(%27)","'"},
-  {R"(%28)","("},
-  {R"(%29)",")"},
-  {R"(%2A)","*"},
-  {R"(%2B)","+"},
-  {R"(%2C)",","},
-  {R"(%3B)",";"},
-  {R"(%3D)","="},
-  {R"(%25)","%"},
-  {R"(%20)"," "},
-  {R"(\+)"," "}
-};
+    {R"(%3A)", ":"}, {R"(%3A)", ":"}, {R"(%2F)", "/"}, {R"(%3F)", "?"},
+    {R"(%23)", "#"}, {R"(%5B)", "["}, {R"(%5D)", "]"}, {R"(%40)", "@"},
+    {R"(%21)", "!"}, {R"(%24)", "$"}, {R"(%26)", "&"}, {R"(%27)", "'"},
+    {R"(%28)", "("}, {R"(%29)", ")"}, {R"(%2A)", "*"}, {R"(%2B)", "+"},
+    {R"(%2C)", ","}, {R"(%3B)", ";"}, {R"(%3D)", "="}, {R"(%25)", "%"},
+    {R"(%20)", " "}, {R"(\+)", " "}};
 
-std::string url_decode(const std::string& str){
+std::string url_decode(const std::string &str) {
   std::string output = str;
-  for(const auto&[encoding,decoding] : url_decode_map){
+  for (const auto &[encoding, decoding] : url_decode_map) {
     output = std::regex_replace(output, std::regex(encoding), decoding);
   }
   return output;
 }
+} // namespace HTTP
